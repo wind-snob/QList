acadd77da7655e1817092a34efa508877098cbc8
Wind Snob <Wind Snob>
2018-01-25 19:56:53 +0000

    DescriptionCell

---
+++ QList/DescriptionCell.swift
@@ -0,0 +1,26 @@
+//
+//  DescriptionCell.swift
+//  QList
+//
+//  Created by Home on 1/25/18.
+//  Copyright © 2018 Home. All rights reserved.
+//
+
+import UIKit
+
+class DescriptionCell: UITableViewCell {
+
+    @IBOutlet weak var itemLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
